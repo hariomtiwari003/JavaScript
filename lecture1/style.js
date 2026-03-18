9122b2d4a132247faa8ee824398844ec41734c31
hariotiwari003 <hariompstiwari@gmail.com>
2026-03-18 19:02:36 +0530

    lerning

---
+++ lecture1/style.js
@@ -0,0 +1,11 @@
+alert('Welcome sir');
+
+
+document.getElementById('bag').addEventListener("click", function() {
+    alert('bag is added');
+});
+
+
+document.getElementById("list").addEventListener("click",  function(){
+    alert("bag is added in wishlist")
+})
